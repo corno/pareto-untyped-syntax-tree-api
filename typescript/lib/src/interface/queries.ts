c06021965bdfcf573a5f5a868254e8f08af9f474
Corno Schraverus <corno@schraverus.com>
2026-07-07 22:44:36 +0200

    pdt: published version 0.1.2

--- typescript/lib/src/interface/queries.ts
+++ typescript/lib/src/interface/queries.ts
@@ -1,4 +1,4 @@
-import * as p_ from 'pareto-core/interface/query'
+import * as p_ from 'pareto-core/interface/query_action'
 
 
 //data types
@@ -6,7 +6,7 @@ import * as d_parse_file from "./data/parse_file.js"
 
 export namespace queries {
 
-    export type parse_file = p_.Query<
+    export type parse_file = p_.Query_Action<
         d_parse_file.Result,
         d_parse_file.Error,
         d_parse_file.Parameters

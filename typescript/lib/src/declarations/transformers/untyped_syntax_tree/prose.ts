5a8228ee85b39a3aac51365efe76fca55eb104f3
Corno Schraverus <corno@schraverus.com>
2026-07-10 10:54:01 +0200

    pdt: published version 0.1.6

--- typescript/lib/src/declarations/transformers/untyped_syntax_tree/prose.ts
+++ typescript/lib/src/declarations/transformers/untyped_syntax_tree/prose.ts
@@ -2,7 +2,7 @@
 import type * as p_ from 'pareto-core/interface/transformer'
 
 //data types
-import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
+import type * as d_out from "pareto-fountain-pen/interface/data/prose"
 import type * as d_in from "../../../interface/data/untyped_syntax_tree.js"
 
 

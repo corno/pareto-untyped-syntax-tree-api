1c436284e211818bdfbc5db50aa42d39741ee7fe
Corno Schraverus <corno@schraverus.com>
2026-07-12 15:50:01 +0200

    pdt: published version 0.1.7

--- typescript/lib/src/implementation/transformers/untyped_syntax_tree/prose.ts
+++ typescript/lib/src/implementation/transformers/untyped_syntax_tree/prose.ts
@@ -1,11 +1,21 @@
 import * as p_ from 'pareto-core/implementation/transformer'
 
-import type * as interface_ from "../../../declarations/transformers/untyped_syntax_tree/prose.js"
+import type * as s_in from "../../../interface/schemas/untyped_syntax_tree.js"
+import type * as s_out from "../../../interface/schemas/prose.js"
+namespace declarations {
+    export type Node = p_.Transformer_With_Parameter<
+        s_in.Node,
+        s_out.Phrase,
+        {
+        'depth': number
+    }
+    >
+}
 
 //shorthands
 import * as sh from "pareto-fountain-pen/shorthands/prose/deprecated"
 
-export const Node: interface_.Node = ($, $p) => $p.depth === 0
+export const Node: declarations.Node = ($, $p) => $p.depth === 0
     ? sh.ph.literal("...") :
     sh.ph.composed([
         sh.ph.literal(`Node: ${$.kind}`),

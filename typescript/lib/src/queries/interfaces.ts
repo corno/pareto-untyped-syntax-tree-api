6424d860d0d68f13a09c72c40773518c433977fe
Corno Schraverus <corno@schraverus.com>
2026-08-11 21:40:59 +0200

    pdt: ...

--- typescript/lib/src/queries/interfaces.ts
+++ typescript/lib/src/queries/interfaces.ts
@@ -3,16 +3,9 @@ import * as p_ from 'pareto-core/interface/query_interface'
 //schemas
 import type * as s_parse_file from "../schemas/parse_file/schema.js"
 
-export namespace queries {
 
-    export type parse_file = p_.Query_Interface<
-        s_parse_file.Result,
-        s_parse_file.Error,
-        s_parse_file.Parameters
-    >
-
-}
-
-export namespace functions {
-
-}
+export type parse_file = p_.Query_Interface<
+    s_parse_file.Result,
+    s_parse_file.Error,
+    s_parse_file.Parameters
+>
